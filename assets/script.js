9696daf0aef87facc63843fe8b48f23ce5f75652
Joyce Chen <joyceideas@outlook.com>
2022-08-04 16:13:52 -0400

    rewriting code in javascript and html, amd adding screenshot

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,38 @@
-//Using jQuery
+//When the saveBtn is clicked, we need to save the values from the input in this local storage, with a key name that matched the hour timeblock, 
+$(".saveBtn").on("click", function(){
+console.log(this);
+var inputvalue=$(this).siblings(".description").val();
+console.log(inputvalue);
 
-$(document).ready(function () {// tells engine to load 1)html & 2)css first.
-    console.log("Save your task!");
-    //    or
-    $(function () {
-        console.log("Save your task!");
-    });
+var keyvalue=$(this).parent().attr("id");
+console.log(keyvalue);
 
+var inputEl=$(this).siblings(".description")
+console.log(inputEl);
+//jQuery to set items in the local storage
+localStorage.setItem(keyvalue,inputvalue);
 
-    //display current day & time. Using moment.js library
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
-    
-    
-    //assign saveBtn click listener for user input and time stamp??
-    $(".saveBtn").on("click", function () {
-        //get nearby values.
-        console.log(this);
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-
-        //set items in local storage.
-        localStorage.setItem(time, text);
-    })
-
-    // jQuery
-    //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
-    $("#8am .description").val(localStorage.getItem("8am"));
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
-    $("#6pm .description").val(localStorage.getItem("6pm"));
-    function hourTracker() {
-        //get current number of hours.
-        var currentHour = moment().hour(); // use of moment.js
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log(blockHour, currentHour)
-
-            //check if we've moved past this time, click into css/html given classes of past, present, or future
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (blockHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-    hourTracker(); //re-run function
+// //jQuery to get the corrent ID of a data from the local storage. 
+// Reference link: https://www.codegrepper.com/code-examples/javascript/javascript+localstorage+getitem //
+// Refeeence link: https://www.w3schools.com/jsref/prop_win_localstorage.asp//
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
+$("#6pm .description").val(localStorage.getItem("6pm"));
 })
+
+
+//get current number of hours.
+// loop over time blocks
+
+
+
+
+
+// var saveBtn=document.querySelectorAll(".saveBtn"); Same as $(".saveBtn")
