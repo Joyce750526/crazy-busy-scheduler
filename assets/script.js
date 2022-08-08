9d56fd43e14cd6d3fefbd345e7646cc76ac8d551
Joyce Chen <joyceideas@outlook.com>
2022-08-08 01:22:49 -0400

    rewriting codes in JS

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,33 @@
- //display current day & time. Using moment.js library
- $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+//display current day & time. Using moment.js library
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 //When the saveBtn is clicked, we need to save the values from the input in this local storage, with a key name that matched the hour timeblock, 
-$(".saveBtn").on("click", function(){
-console.log(this);
-var inputvalue=$(this).siblings(".description").val();
-console.log(inputvalue);
+$(".saveBtn").on("click", function () {
+    console.log(this);
+    var inputvalue = $(this).siblings(".description").val();
+    console.log(inputvalue);
 
-var keyvalue=$(this).parent().attr("id");
-console.log(keyvalue);
+    var keyvalue = $(this).parent().attr("id");
+    console.log(keyvalue);
 
-var inputEl=$(this).siblings(".description")
-console.log(inputEl);
-//jQuery to set items in the local storage
-localStorage.setItem(keyvalue,inputvalue);
+    var inputEl = $(this).siblings(".description")
+    console.log(inputEl);
+    //jQuery to set items in the local storage
+    localStorage.setItem(keyvalue, inputvalue);
 
-// //jQuery to get the corrent ID of a data from the local storage. 
-// Reference link: https://www.codegrepper.com/code-examples/javascript/javascript+localstorage+getitem //
-// Refeeence link: https://www.w3schools.com/jsref/prop_win_localstorage.asp//
-$("#9am .description").val(localStorage.getItem("9am"));
-$("#10am .description").val(localStorage.getItem("10am"));
-$("#11am .description").val(localStorage.getItem("11am"));
-$("#12pm .description").val(localStorage.getItem("12pm"));
-$("#1pm .description").val(localStorage.getItem("1pm"));
-$("#2pm .description").val(localStorage.getItem("2pm"));
-$("#3pm .description").val(localStorage.getItem("3pm"));
-$("#4pm .description").val(localStorage.getItem("4pm"));
-$("#5pm .description").val(localStorage.getItem("5pm"));
-$("#6pm .description").val(localStorage.getItem("6pm"));
+    // //jQuery to get the corrent ID of a data from the local storage. 
+    // Reference link: https://www.codegrepper.com/code-examples/javascript/javascript+localstorage+getitem //
+    // Refeeence link: https://www.w3schools.com/jsref/prop_win_localstorage.asp//
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#6pm .description").val(localStorage.getItem("6pm"));
 })
 
 //get current number of hours.
@@ -37,31 +37,29 @@ function hourTracker() {
     //get current number of hours.
     var currentHour = moment().hour(); // use of moment.js
 
+
     // loop over time blocks
     $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockHour, currentHour)
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockHour, currentHour);
 
         //check if we've moved past this time, click into css/html given classes of past, present, or future
         if (blockHour < currentHour) {
             $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        else if (blockHour === currentHour) {
+        } else if (blockHour === currentHour) {
             $(this).removeClass("past");
             $(this).addClass("present");
             $(this).removeClass("future");
-        }
-        else {
+        } else {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
-        
+        }
+    });
 }
-hourTracker(); //re-run function
-})
-}
+
+hourTracker(); //re-run 
+
 
 
 // var saveBtn=document.querySelectorAll(".saveBtn"); Same as $(".saveBtn")
